816d7b3d10f54586e0bba26f289d7daf392bdc8e
soca-nunca <guigorocha09@gmail.com>
2025-04-09 14:20:29 -0300

    att

--- c.py
+++ c.py
@@ -13,3 +13,21 @@ elif meio == '/':
     print (numero/numero2)
 else:
     print ("inválido")
+
+postp = input('Quer continuar?')
+if postp =='sim':
+    numero = int(input("insira um valor para fazer uma conta"))
+numero2 = int(input("insira outro valor para fazer a conta"))
+
+meio = input("escolha o tipo de conta")
+
+if meio == '+':
+    print (numero+numero2)
+elif meio == '-':
+    print (numero2-numero)
+elif meio == '*':
+    print (numero*numero2)
+elif meio == '/':
+    print (numero/numero2)
+else:
+    print ("inválido")
