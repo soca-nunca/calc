2618c84794971823704af3ed5772849f1028a4a6
soca-nunca <guigorocha09@gmail.com>
2025-04-09 14:11:36 -0300

    att

---
+++ c.py
@@ -0,0 +1,15 @@
+numero = input("insira um valor para fazer uma conta")
+numero2 = input("insira outro valor para fazer a conta")
+
+meio = input("escolha o tipo de conta")
+
+if meio == '+':
+    print (numero+numero2)
+elif meio == '-':
+    print (numero2-numero)
+elif meio == '*':
+    print (numero*numero2)
+elif meio == '/':
+    print (numero/numero2)
+else:
+    print ("inválido")
